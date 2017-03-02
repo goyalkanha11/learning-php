842f28573f5c65dc7540dd6f57a0d7c0caa3c7be
Kanha Goyal <goyalkanha11@gmail.com>
2017-03-03 02:11:05 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php $Name = $_POST["Name"];
+$Branch = $_POST["Branch"];
+$Year = $_POST["Year"];
+$welcome = "Welcome $Name you are in $Branch Branch and in $Year  year";
+$subject = "Contact From";
+
+echo $welcome;
+echo "Thankyou";
+?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="mail.php" method="POST">
+<P>Name</P><input type="text" name="Name">
+<p>Branch</p><input type="text" name="Branch">
+<p>Year</p><input type="text" name="Year">
+<input type="submit" value="send"><input type="reset" value="clear">
+</form>
+</body>
+</html>
